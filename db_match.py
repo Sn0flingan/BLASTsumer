8009b3258f6373d14ba5e158dd3f2299194eaa9c
Sn0flingan <alice.anlind@gmail.com>
2018-06-27 15:54:24 +0200

    Add skeleton of db_match class

---
+++ db_match.py
@@ -0,0 +1,11 @@
+class db_match:
+    short_name = ""
+    name = ""
+    count = 0
+    pid = []
+    alg_len = []
+    e_val= []
+    missmatch = []
+    gaps = []
+    gap_openings = []
+    reads = []
